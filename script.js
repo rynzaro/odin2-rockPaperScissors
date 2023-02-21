d8ae6d6ee422bbb7c674de05a382123fdbb5338e
EfteYo <58327339+EfteYo@users.noreply.github.com>
2023-02-21 16:28:53 +0100

    Create play and test function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const choices = ["Rock", "Paper", "Scissors"];
+const choices = ["rock", "paper", "scissors"];
 
 function getRandomNumber(maximum) {
+    // Math.random returns random value between 0 and 1;
     return Math.floor(Math.random() * maximum);
 }
 
@@ -8,4 +9,45 @@ function getComputerChoice() {
     const randomInt = getRandomNumber(3);
     const computerChoice = choices[randomInt];
     return computerChoice;    
+}
+
+function playGame(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    var playerWon;
+    if (playerSelection == computerSelection) {
+        return `It's a draw! Both players chose ${playerSelection}`;
+
+    } else if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            playerWon = false;
+        } else {
+            playerWon = true;
+        }
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            playerWon = true;
+        } else {
+            playerWon = false;
+        }
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+            playerWon = false;
+        } else {
+            playerWon = true;
+        }
+    } else {
+        return "Your choice was invalid, please choose (case insensitive) rock, paper or scissors!";
+    }
+    return playerWon ? `You won! ${playerSelection} wins against ${computerSelection}` : `You lost! ${playerSelection} lost against ${computerSelection}`
+}
+
+function test() {
+    function printAndPlay(playerChoice) {
+        console.log(`Player chooses ${playerChoice}`);
+        console.log(playGame(playerChoice, getComputerChoice()));
+    }
+
+    printAndPlay("rock");
+    printAndPlay("rOCk");
+    printAndPlay("rokk");
 }
