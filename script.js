608e9c46e36ebb232a449db1cd2acb7f3b1dc51b
EfteYo <58327339+EfteYo@users.noreply.github.com>
2023-02-21 16:56:09 +0100

    Create game function, change output format for round function

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const choices = ["rock", "paper", "scissors"];
+var playerSelection;
+var computerSelection;
 
 function getRandomNumber(maximum) {
     // Math.random returns random value between 0 and 1;
@@ -11,40 +13,77 @@ function getComputerChoice() {
     return computerChoice;    
 }
 
-function playGame(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
-    var playerWon;
+    var result;
     if (playerSelection == computerSelection) {
-        return `It's a draw! Both players chose ${playerSelection}`;
+        return 0;
 
     } else if (playerSelection === "rock") {
         if (computerSelection === "paper") {
-            playerWon = false;
+            result = -1;
         } else {
-            playerWon = true;
+            result = 1;
         }
     } else if (playerSelection === "paper") {
         if (computerSelection === "rock") {
-            playerWon = true;
+            result = 1;
         } else {
-            playerWon = false;
+            result = -1;
         }
     } else if (playerSelection === "scissors") {
         if (computerSelection === "rock") {
-            playerWon = false;
+            result = -1;
         } else {
-            playerWon = true;
+            result = 1;
         }
     } else {
         return "Your choice was invalid, please choose (case insensitive) rock, paper or scissors!";
     }
-    return playerWon ? `You won! ${playerSelection} wins against ${computerSelection}` : `You lost! ${playerSelection} lost against ${computerSelection}`
+    return result;
+}
+
+function game() {
+    var playerScore = 0;
+    var computerScore = 0;
+
+    while((playerScore < 5) && (computerScore < 5)) {
+
+        playerSelection = prompt("Choose rock, paper or scissors, case insensitive").toLowerCase();
+        // while loop until input is in right format
+        while (
+            (playerSelection !== "rock") &&
+            (playerSelection !== "paper") &&
+            (playerSelection !== "scissors")
+        ) {
+            playerSelection = prompt("Choose rock, paper or scissors, case insensitive").toLowerCase();
+        }
+        computerSelection = getComputerChoice();
+        const result = playRound(playerSelection, computerSelection);
+
+        if (result > 0) { 
+            playerScore++; 
+            console.log(`You won! ${playerSelection} beats ${computerSelection}`);
+        } else if (result < 0) { 
+            computerScore++; 
+            console.log(`You lost! ${playerSelection} lost against ${computerSelection}`);
+        } else {
+            console.log(`It's a tie! You both picked ${playerSelection}`);
+        }
+        console.log(`score is ${playerScore} to ${computerScore}`);
+    }
+
+    if (playerScore === 5) {
+        console.log(`You won ${playerScore} to ${computerScore}! Congratulations!`);
+    } else {
+        console.log(`You lost... but you got ${playerScore} points along the way!`);
+    }
 }
 
 function test() {
     function printAndPlay(playerChoice) {
         console.log(`Player chooses ${playerChoice}`);
-        console.log(playGame(playerChoice, getComputerChoice()));
+        console.log(playRound(playerChoice, getComputerChoice()));
     }
 
     printAndPlay("rock");
